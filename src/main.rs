52627a73e082a78b3336692e471dcc716eae297b
nishantattrey07 <nishantattrey07@gmail.com>
2026-05-21 10:53:06 +0530

    Parsed Raw Text Into a Typed Command

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,93 @@
-use std::{io::Read, net::{TcpListener, TcpStream}};
+use std::{char, io::Read, net::{TcpListener, TcpStream}};
 
+// enum Command<'a> {
+//     Set {
+//         key: &'a str,
+//         value: &'a str,
+//     },
 
-fn process_command(command: &str) {
-    println!("COMMAND: {}", command);
+//     Get {
+//         key: &'a str,
+//     },
+
+//     Delete {
+//         key: &'a str,
+//     },
+// }
+
+#[derive(Debug)]
+enum Command {
+    Set {
+        key: String,
+        value: String,
+    },
+
+    Get {
+        key: String,
+    },
+
+    Delete {
+        key: String,
+    },
+}
+
+fn process_command(input: &str) -> Result<Command, String> {
+    println!("COMMAND: {}", input);
+
+    
+    let input = input.trim_start();
+
+    // split command from rest
+    let mut split_data = input.splitn(2, ' ');
+
+    let command = split_data
+        .next()
+        .ok_or("Command is empty".to_string())?;
+
+    match command.to_uppercase().as_str() {
+        "SET" => {
+            let rest = split_data
+                .next()
+                .ok_or("SET requires key and value".to_string())?;
+
+            let mut set_parts = rest.trim_start().splitn(2, ' ');
+
+            let key = set_parts
+                .next()
+                .ok_or("Missing key".to_string())?;
+
+            let value = set_parts
+                .next()
+                .ok_or("Missing value".to_string())?;
+
+            Ok(Command::Set {
+                key: key.to_string(),
+                value: value.to_string(),
+            })
+        }
+
+        "GET" => {
+            let key = split_data
+                .next()
+                .ok_or("GET requires key".to_string())?;
+
+            Ok(Command::Get {
+                key: key.to_string(),
+            })
+        }
+
+        "DELETE" => {
+            let key = split_data
+                .next()
+                .ok_or("DELETE requires key".to_string())?;
+
+            Ok(Command::Delete {
+                key: key.to_string(),
+            })
+        }
+
+        _ => Err("Unknown command".to_string()),
+    }
 }
 
 fn handle_client(stream: &mut TcpStream) -> std::io::Result<()> {
@@ -22,7 +107,7 @@ fn handle_client(stream: &mut TcpStream) -> std::io::Result<()> {
 
         for byte in buf[..bytes_read].iter() {
             bytes.push(*byte);
-            println!("{:?}",bytes);
+            // println!("{:?}",bytes);
 
             // newline delimiter
             if *byte as char == '\n' {
@@ -33,8 +118,16 @@ fn handle_client(stream: &mut TcpStream) -> std::io::Result<()> {
                 // bytes -> String
                 let command = String::from_utf8_lossy(&bytes);
 
-                process_command(&command);
+              let response = process_command(&command);
 
+              match response {
+                  Ok(command) => {
+                      println!("command: {:#?}",command);
+                  },
+                  Err(err) => {
+                      eprintln!("{}",err);
+                  }
+              }
                 // ready for next command
                 bytes.clear();
             }
